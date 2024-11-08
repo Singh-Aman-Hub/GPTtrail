826292bcae80ece75237302640aaf8393b10dbe4
Aman Singh <amansingh102006@gmail.com>
2024-11-08 21:49:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('registrationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for registering! We will contact you shortly.');
+});
